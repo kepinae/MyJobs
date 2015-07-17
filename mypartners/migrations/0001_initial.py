17ae9b5722ff824f480f602c3d1122408f0ed368
aspidites <emarshall85@gmail.com>
2015-07-17 11:00:35 -0400

    migrations - mypartners

---
+++ mypartners/migrations/0001_initial.py
@@ -0,0 +1,282 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import mypartners.models
+import django.db.models.deletion
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0001_initial'),
+        ('seo', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Contact',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name=b'Full Name')),
+                ('email', models.EmailField(max_length=255, verbose_name=b'Email', blank=True)),
+                ('phone', models.CharField(default=b'', max_length=30, verbose_name=b'Phone', blank=True)),
+                ('notes', models.TextField(default=b'', max_length=1000, verbose_name=b'Notes', blank=True)),
+                ('archived_on', models.DateTimeField(null=True)),
+            ],
+            options={
+                'verbose_name_plural': 'contacts',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ContactLogEntry',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('action_flag', models.PositiveSmallIntegerField(verbose_name=b'action flag')),
+                ('action_time', models.DateTimeField(auto_now=True, verbose_name=b'action time')),
+                ('change_message', models.TextField(verbose_name=b'change message', blank=True)),
+                ('contact_identifier', models.CharField(max_length=255)),
+                ('delta', models.TextField(blank=True)),
+                ('object_id', models.TextField(null=True, verbose_name=b'object id', blank=True)),
+                ('object_repr', models.CharField(max_length=200, verbose_name=b'object repr')),
+                ('content_type', models.ForeignKey(blank=True, to='contenttypes.ContentType', null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ContactRecord',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_on', models.DateTimeField(auto_now=True)),
+                ('contact_type', models.CharField(max_length=50, verbose_name=b'Contact Type', choices=[(b'email', b'Email'), (b'phone', b'Phone'), (b'meetingorevent', b'Meeting or Event'), (b'job', b'Job Followup'), (b'pssemail', b'Partner Saved Search Email')])),
+                ('contact_email', models.CharField(max_length=255, verbose_name=b'Contact Email', blank=True)),
+                ('contact_phone', models.CharField(default=b'', max_length=30, verbose_name=b'Contact Phone Number', blank=True)),
+                ('location', models.CharField(default=b'', max_length=255, verbose_name=b'Meeting Location', blank=True)),
+                ('length', models.TimeField(null=True, verbose_name=b'Meeting Length', blank=True)),
+                ('subject', models.CharField(default=b'', max_length=255, verbose_name=b'Subject or Topic', blank=True)),
+                ('date_time', models.DateTimeField(verbose_name=b'Date & Time', blank=True)),
+                ('notes', models.TextField(default=b'', max_length=1000, verbose_name=b'Details, Notes or Transcripts', blank=True)),
+                ('job_id', models.CharField(default=b'', max_length=40, verbose_name=b'Job Number/ID', blank=True)),
+                ('job_applications', models.CharField(default=b'', max_length=6, verbose_name=b'Number of Applications', blank=True)),
+                ('job_interviews', models.CharField(default=b'', max_length=6, verbose_name=b'Number of Interviews', blank=True)),
+                ('job_hires', models.CharField(default=b'', max_length=6, verbose_name=b'Number of Hires', blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Location',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('label', models.CharField(max_length=60, verbose_name=b'Address Label', blank=True)),
+                ('address_line_one', models.CharField(max_length=255, verbose_name=b'Address Line One', blank=True)),
+                ('address_line_two', models.CharField(max_length=255, verbose_name=b'Address Line Two', blank=True)),
+                ('city', models.CharField(max_length=255, verbose_name=b'City')),
+                ('state', models.CharField(max_length=200, verbose_name=b'State/Region')),
+                ('country_code', models.CharField(default=b'USA', max_length=3, verbose_name=b'Country')),
+                ('postal_code', models.CharField(max_length=12, verbose_name=b'Postal Code', blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Partner',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, verbose_name=b'Partner Organization')),
+                ('data_source', models.CharField(max_length=255, verbose_name=b'Source', blank=True)),
+                ('uri', models.URLField(verbose_name=b'URL', blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='PartnerLibrary',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('data_source', models.CharField(default=b'Employment Referral Resource Directory', max_length=255)),
+                ('name', models.CharField(max_length=255, verbose_name=b'Partner Organization')),
+                ('uri', models.URLField(blank=True)),
+                ('region', models.CharField(max_length=30, blank=True)),
+                ('state', models.CharField(max_length=30, blank=True)),
+                ('area', models.CharField(max_length=255, blank=True)),
+                ('contact_name', models.CharField(max_length=255, blank=True)),
+                ('phone', models.CharField(max_length=30, blank=True)),
+                ('phone_ext', models.CharField(max_length=10, blank=True)),
+                ('alt_phone', models.CharField(max_length=30, blank=True)),
+                ('fax', models.CharField(max_length=30, blank=True)),
+                ('email', models.CharField(max_length=255, blank=True)),
+                ('street1', models.CharField(max_length=255, blank=True)),
+                ('street2', models.CharField(max_length=255, blank=True)),
+                ('city', models.CharField(max_length=255, blank=True)),
+                ('st', models.CharField(max_length=10, blank=True)),
+                ('zip_code', models.CharField(max_length=12, blank=True)),
+                ('is_minority', models.BooleanField(default=False, verbose_name=b'minority')),
+                ('is_female', models.BooleanField(default=False, verbose_name=b'female')),
+                ('is_disabled', models.BooleanField(default=False, verbose_name=b'disabled')),
+                ('is_veteran', models.BooleanField(default=False, verbose_name=b'veteran')),
+                ('is_disabled_veteran', models.BooleanField(default=False, verbose_name=b'disabled_veteran')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='PRMAttachment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('attachment', models.FileField(max_length=767, null=True, upload_to=mypartners.models.get_file_name, blank=True)),
+                ('contact_record', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='mypartners.ContactRecord', null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Status',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('code', models.PositiveSmallIntegerField(default=1, verbose_name=b'Status Code', choices=[(0, b'Unprocessed'), (1, b'Approved'), (2, b'Denied')])),
+                ('last_modified', models.DateTimeField(auto_now=True, verbose_name=b'Last Modified')),
+                ('approved_by', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('hex_color', models.CharField(default=b'd4d4d4', max_length=6, blank=True)),
+                ('created_on', models.DateTimeField(auto_now=True)),
+                ('company', models.ForeignKey(to='seo.Company')),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': 'tag',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterUniqueTogether(
+            name='tag',
+            unique_together=set([('name', 'company')]),
+        ),
+        migrations.AddField(
+            model_name='partner',
+            name='approval_status',
+            field=models.OneToOneField(null=True, verbose_name=b'Approval Status', to='mypartners.Status'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='partner',
+            name='library',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='mypartners.PartnerLibrary', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='partner',
+            name='owner',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='seo.Company', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='partner',
+            name='primary_contact',
+            field=models.ForeignKey(related_name='primary_contact', on_delete=django.db.models.deletion.SET_NULL, to='mypartners.Contact', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='partner',
+            name='tags',
+            field=models.ManyToManyField(to='mypartners.Tag', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contactrecord',
+            name='approval_status',
+            field=models.OneToOneField(null=True, verbose_name=b'Approval Status', to='mypartners.Status'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contactrecord',
+            name='contact',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='mypartners.Contact', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contactrecord',
+            name='created_by',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contactrecord',
+            name='partner',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='mypartners.Partner', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contactrecord',
+            name='tags',
+            field=models.ManyToManyField(to='mypartners.Tag', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contactlogentry',
+            name='partner',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='mypartners.Partner', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contactlogentry',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='approval_status',
+            field=models.OneToOneField(null=True, verbose_name=b'Approval Status', to='mypartners.Status'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='library',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='mypartners.PartnerLibrary', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='locations',
+            field=models.ManyToManyField(related_name='contacts', to='mypartners.Location'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='partner',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, to='mypartners.Partner', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='tags',
+            field=models.ManyToManyField(to='mypartners.Tag', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='contact',
+            name='user',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, blank=True, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+    ]
