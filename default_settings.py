5686f7bf7112495fa32e7036bef31e3722af8618
aspidites <emarshall85@gmail.com>
2015-07-17 14:44:38 -0400

    Updated how myemails is imported.

--- default_settings.py
+++ default_settings.py
@@ -134,9 +134,9 @@ CELERY_QUEUES = {
     'sendgrid': {
         'binding_key': 'sendgrid.#'
     },
-    #'myemails': {
-    #    'binding_key': 'myemails.#'
-    #}
+    'myemails': {
+        'binding_key': 'myemails.#'
+    }
 }
 CELERY_ROUTES = {
     'tasks.task_update_solr': {
@@ -188,10 +188,10 @@ CELERY_ROUTES = {
         'queue': 'myjobs',
         'routing_key': 'dseo.submit_all_sitemaps'
     },
-    #'tasks.send_event_email': {
-    #    'queue': 'myemails',
-    #    'routing_key': 'myemails.send_event_emails'
-    #},
+    'tasks.send_event_email': {
+        'queue': 'myemails',
+        'routing_key': 'myemails.send_event_emails'
+    },
     'tasks.process_sendgrid_event': {
         'queue': 'sendgrid',
         'routing_key': 'sendgrid.process_sendgrid_event',
@@ -285,7 +285,7 @@ CAPTCHA_AJAX = True
 PROJECT_APPS = ('myjobs', 'myprofile', 'mysearches', 'registration',
                 'mydashboard', 'mysignon', 'mymessages', 'mypartners',
                 'solr', 'postajob', 'moc_coding', 'seo', 'social_links',
-                'wildcard', 'myblocks', #'myemails', 
+                'wildcard', 'myblocks', 'myemails', 
                 'myreports', 'redirect', 'automation')
 
 INSTALLED_APPS += PROJECT_APPS
